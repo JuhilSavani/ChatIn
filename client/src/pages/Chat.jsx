0fc664d78bbf8bdcbbd98de8b318d54c775f3f5b
JuhilSavani <savanijuhil@gmail.com>
2024-11-23 22:32:00 +0530

    add initial chat page

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="page chat">
+      <div className="container">Chat</div>
+    </div>
+  );
+};
+
+export default Chat;
