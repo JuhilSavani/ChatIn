bc0fa223c50ce6d19e87b7a48dfa4b573674b0ab
JuhilSavani <savanijuhil@gmail.com>
2024-11-23 22:32:40 +0530

    add loading component

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="page loading">
+      <div className="container">
+        <h1>Loading</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
