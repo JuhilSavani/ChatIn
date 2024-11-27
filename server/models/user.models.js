6b5f89bb22c0d9fa86600a56434ce16df9f4e985
JuhilSavani <savanijuhil@gmail.com>
2024-11-27 10:34:30 +0530

    fix: sequelize syntax issue

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,28 +1,33 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../config/sequelize.config.js"
+import { sequelize } from "../config/sequelize.config.js";
 
-export const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+export const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      unique: false,
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: false,
+    },
   },
-  name: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-    unique: false,
-  }, 
-  email: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    unique: false,
-  },
-  tableName: 'users',
-  timestamps: false, 
-});
+  {
+    tableName: "users",
+    timestamps: false,
+  }
+);
