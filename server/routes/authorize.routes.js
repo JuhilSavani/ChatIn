edfa99d8f1f62a8bf6a92770da57904b0993892c
JuhilSavani <savanijuhil@gmail.com>
2024-12-14 15:22:54 +0530

    add authentication route to verify login status of user

--- server/routes/authorize.routes.js
+++ server/routes/authorize.routes.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { login, register, logout, refresh, verifyEmail } from '../controllers/authorize.controllers.js';
+import {
+  login,
+  register,
+  logout,
+  verifyEmail,
+} from "../controllers/authorize.controllers.js";
+import { authenticateJWT } from "../config/passport.config.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
 router.post("/logout", logout);
-router.get("/refresh", refresh);
+router.get("/check", authenticateJWT, (req, res) => {
+  res.status(200).json({ message: "Authenticated", user: req.user });
+});
 router.get("/:email", verifyEmail);
+
 
 export default router;
