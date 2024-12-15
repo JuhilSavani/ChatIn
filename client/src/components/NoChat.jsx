321b065d7779baadca905b5aac0ef8f5f3ec983c
JuhilSavani <savanijuhil@gmail.com>
2024-12-15 11:34:10 +0530

    add panel components for messaging area

---
+++ client/src/components/NoChat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NoChat = () => {
+  return (
+    <div className="no-chat">
+      <span>
+        <i className="bx bxs-message-alt-dots"></i>
+      </span>
+      <h1>Welcome to ChatIn'</h1>
+      <p>Select any conversation from the sidebar to start chatting.</p>
+    </div>
+  );
+};
+
+export default NoChat;
