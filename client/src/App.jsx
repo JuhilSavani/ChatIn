c4762d504507df9d3f7ff59e00f31e3db9c91d44
JuhilSavani <savanijuhil@gmail.com>
2024-12-15 11:26:22 +0530

    update routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,28 @@ import {
   Outlet,
   Route,
   RouterProvider,
+  useLocation,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Chat from "./pages/Chat";
+import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import useAuth from "./utils/hooks/useAuth";
 
 // Define a layout component
 const Layout = () => {
+  const location = useLocation();
+  const { isAuthenticated } = useAuth();
+
+  const navbarRoutes = ["/", "/about", "/home", "/profile"];
+  const showNavbar = navbarRoutes.includes(location.pathname);
+  
   return (
     <>
-      <Navbar />
+      {isAuthenticated && showNavbar && <Navbar />}
       <Outlet />
     </>
   );
@@ -24,10 +35,14 @@ const Layout = () => {
 // Define the routes
 const routes = createRoutesFromElements(
   <Route path="/" element={<Layout />}>
-    {/* Public */}
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="chat" element={<Chat />} />
+    <Route element={<ProtectedRoute />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+
+    <Route path="sign-in" element={<SignIn />} />
+    <Route path="sign-up" element={<SignUp />} />
   </Route>
 );
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,43 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import axios from "../utils/apis/axios";
+import useAuth from "../utils/hooks/useAuth";
 
 const Navbar = () => {
+  const { setIsAuthenticated } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  
+  const logout = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      await axios.post('/authorize/logout');
+      setIsAuthenticated(false);
+      navigate("/sign-in", {replace: true});
+    } catch (error) {
+      console.error(error.stack);
+      alert(error?.response?.data.message || error?.message)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
   return (
     <header>
       <nav>
         <span>
-          <Link>ChatIn'</Link>
+          <Link><i className="bx bxs-message-alt-dots"></i>ChatIn'</Link>
         </span>
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about"><i className='bx bxs-info-square' ></i>About</NavLink>
           </li>
           <li>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/profile"><i className='bx bxs-user-account' ></i>Profile</NavLink>
+          </li>
+          <li>
+            <button onClick={logout}><i className='bx bxs-right-arrow-square'></i>{isLoading ? "Exiting" : "Logout"}</button>
           </li>
         </ul>
       </nav>
