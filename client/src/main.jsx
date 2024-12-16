1f9339e8af1360e49dd64e7dac924d21f6b2127c
JuhilSavani <savanijuhil@gmail.com>
2024-12-16 17:05:18 +0530

    set up global context provider to react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,17 @@ import "./styles.scss";
 import App from "./App.jsx";
 import { AuthProvider } from "./utils/contexts/AuthContext.jsx";
 import { SocketProvider } from "./utils/contexts/SocketContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <AuthProvider>
     <SocketProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </SocketProvider>
   </AuthProvider>
   // </StrictMode>
