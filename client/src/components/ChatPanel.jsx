cf318c4c94e0d3ff31ec52bbf277b11f7b65696e
JuhilSavani <savanijuhil@gmail.com>
2024-12-16 17:15:42 +0530

    complete sidebar for home page and set up chat panel

--- client/src/components/ChatPanel.jsx
+++ client/src/components/ChatPanel.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const ChatPanel = () => {
-  return <div>ChatPanel</div>;
+const ChatPanel = ({ contact }) => {
+  return (
+    <div className="chat-panel">
+      <section className="contact-info">
+        <i className='bx bx-user-circle'></i>
+        <div>
+          <h2>{contact.connectedUser.name}</h2>
+          <span>{contact.connectedUser.email}</span>
+        </div>
+      </section>
+      <section className="chat-area"></section>
+      <section className="message-input">
+          <input type="text" />
+          <span><i className='bx bxs-send'></i></span>
+      </section>
+    </div>
+  );
 };
 
 export default ChatPanel;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,38 +1,72 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import NoChat from "../components/NoChat";
+import useAuth from "../utils/hooks/useAuth";
+import addContact from "../utils/controllers/addContact";
+import fetchContacts from "../utils/controllers/fetchContacts";
+import ChatPanel from "../components/ChatPanel";
 
 const Home = () => {
+  const { user } = useAuth();
+  const { data, isLoading, isError, error } = fetchContacts();
   const dialogRef = useRef(null);
+
+  const [selectedContact, selectContact] = useState({});
+  console.log(Boolean(selectedContact));
+
+  const { mutate: addContactMutate, isLoading: isAdding } = addContact();
 
   const openDialog = () => dialogRef.current?.showModal();
   const closeDialog = () => dialogRef.current?.close();
 
-  const handleSubmit = (e) => {
+  const handleCreate = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
-    // todo: connect to the email `[post] /connection/add` with react-query
-    console.log("Email submitted:", email);
+    try {
+      if (email == user.email)
+        throw new Error("Please provide a valid email other than yours!");
+      addContactMutate({ userId: user.id, email });
+    } catch (error) {
+      alert(error?.response?.data.message || error.message);
+      console.error(error?.response?.data.stack || error.stack);
+    }
     closeDialog();
   };
-  
+
   return (
     <div className="page home">
       <div className="container">
         <section className="sidebar">
-          <div>
+          <div className="sidebar-header">
             <span>
               <i className="bx bxs-contact"></i>Contacts
             </span>
-            <button className="add-btn" onClick={openDialog}>
+            <button className="dialog-btn" onClick={openDialog}>
               <i className="bx bxs-user-plus"></i>
             </button>
           </div>
-          <div></div>
+          <div className="contacts">
+            <ul>
+              {isLoading && (<li className="sidebar-msg"><p><strong>Loading connections...</strong></p></li>)}
+              {isError && (<li className="sidebar-msg"><p><strong>Error: </strong> {error.message}</p></li>)}
+              {!isLoading && !isError && !Boolean(data?.length) && (<li className="sidebar-msg"><p><strong>No contacts yet!</strong></p></li>)}
+              {!isLoading && !isError && Boolean(data?.length) &&
+                data.map((c) => (
+                  <li key={c.id} onClick={() => selectContact(c)}>
+                    <i className="bx bx-user-circle"></i>
+                    <div>
+                      <h2>{c.connectedUser.name}</h2>
+                      <span>{c.connectedUser.email}</span>
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         </section>
         <dialog ref={dialogRef} className="modal">
           <div className="modal-content">
             <h3>Add Contact</h3>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleCreate}>
               <label>
                 Email:
                 <input
@@ -46,15 +80,19 @@ const Home = () => {
                 <button className="cancel-btn" onClick={closeDialog}>
                   Cancel
                 </button>
-                <button type="submit" className="add-btn">
-                  Add
+                <button
+                  type="submit"
+                  className="create-btn"
+                  disabled={isAdding}
+                >
+                  {isAdding ? "..." : "Add"}
                 </button>
               </div>
             </form>
           </div>
         </dialog>
         <section className="panel">
-          <NoChat />
+          {selectedContact?.status ? <ChatPanel contact={selectedContact}/> : <NoChat />}
         </section>
       </div>
     </div>
@@ -62,5 +100,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
