24904664573f5b9a7a3249e9bb6d2ae14da8d0f4
JuhilSavani <savanijuhil@gmail.com>
2024-12-16 17:10:12 +0530

    update controllers

--- server/controllers/connection.controllers.js
+++ server/controllers/connection.controllers.js
@@ -1,6 +1,19 @@
 import { Connection } from "../models/connection.models.js";
 import { User } from "../models/user.models.js";
-import { Op } from "sequelize"; 
+import { Op } from "sequelize";
+
+export const computedConnection = (userId, connection) => {
+  const connectedUser = connection.user1.id === parseInt(userId) ? connection.user2 : connection.user1;
+  return {
+    connectionId: connection.id,
+    status: connection.status,
+    connectedUser: {
+      id: connectedUser.id,
+      name: connectedUser.name,
+      email: connectedUser.email,
+    },
+  };
+};
 
 export const getConnections = async (req, res) => {
   const { userId } = req.params;
@@ -28,35 +41,12 @@ export const getConnections = async (req, res) => {
       },
       attributes: ["id", "status"],
       include: [
-        {
-          model: User,
-          as: "user1",
-          attributes: ["id", "name", "email"],
-        },
-        {
-          model: User,
-          as: "user2",
-          attributes: ["id", "name", "email"],
-        },
+        { model: User, as: "user1", attributes: ["id", "name", "email"] },
+        { model: User, as: "user2", attributes: ["id", "name", "email"] },
       ],
     });
 
-    const result = connections.map((connection) => {
-      // Determine the connected user
-      const connectedUser =
-        connection.userId1 === parseInt(userId, 10)
-          ? connection.user2
-          : connection.user1;
-
-      return {
-        connectionId: connection.id,
-        connectedUser: {
-          id: otherUser.id,
-          name: connectedUser.name,
-          email: connectedUser.email,
-        },
-      };
-    });
+    const result = connections.map((connection) => computedConnection(userId, connection));
     return res.status(200).json(result);
   } catch (error) {
     console.error(
@@ -71,50 +61,72 @@ export const getConnections = async (req, res) => {
 };
 
 export const createConnection = async (req, res) => {
-  const { userId1, userId2 } = req.body;
+  const { userId, email } = req.body;
   try {
-    if (!userId1 || !userId2)
-      return res.status(400).json({ message: "Both user IDs are required to establish a connection." });
+    if (!userId || !email)
+      return res
+        .status(400)
+        .json({
+          message:
+            "One or both required fields are missing to establish a connection.",
+        });
 
-    const [user1, user2] = await Promise.all([
-      User.findByPk(userId1),
-      User.findByPk(userId2),
-    ]);
+    const user1 = await User.findByPk(userId);
+    const user2 = await User.findOne({ where: { email } });
 
-    if(!user1 || !user2)
+    if (!user1 || !user2)
       return res.status(404).json({ message: "One or both users not found." });
 
     const existingConnection = await Connection.findOne({
       where: {
         [Op.or]: [
-          { userId1: userId1, userId2: userId2 },
-          { userId1: userId2, userId2: userId1 },
+          { userId1: user1.id, userId2: user2.id },
+          { userId1: user2.id, userId2: user1.id },
         ],
       },
+      attributes: ["id", "status"],
+      include: [
+        { model: User, as: "user1", attributes: ["id", "name", "email"] },
+        { model: User, as: "user2", attributes: ["id", "name", "email"] },
+      ],
     });
 
-    if (existingConnection){
-      if(existingConnection.status == "blocked")
+    if (existingConnection) {
+      if (existingConnection.status == "blocked")
         return res.status(400).json({ message: "This connection is blocked." });
-      else if(existingConnection.status == "pending"){
+      else if (existingConnection.status == "accepted")
+        return res.status(400).json({ message: "This connection already exists." });
+      else if (existingConnection.user1.id == userId)
+        return res.status(400).json({ message: "This connection already exists." });
+      else {
         existingConnection.status = "accepted";
         await existingConnection.save();
-        return res.status(201).json(existingConnection);
-      }else
-        return res.status(400).json({ message: "This connection is blocked." });
+        const result = computedConnection(user1.id, existingConnection);
+        return res.status(201).json(result);
+      }
     }
-
-    const newConnection = await Connection.create({ userId1, userId2 });
-    return res.status(201).json(newConnection);
+    const newConnection = await Connection.create({ userId1: user1.id, userId2: user2.id });
+    const result = {
+      connectionId: newConnection.id,
+      status: newConnection.status,
+      connectedUser: {
+        id: user2.id,
+        name: user2.name,
+        email: user2.email,
+      },
+    };
+    return res.status(201).json(result);
   } catch (error) {
-    console.error("[connection.controllers.js] Error creating connection: ", error.stack);
+    console.error(
+      "[connection.controllers.js] Error creating connection: ",
+      error.stack
+    );
     return res.status(500).json({
       message: "An error occurred while creating the connection.",
       error: error.message,
     });
   }
-}
-
+};
 
 export const updateConnectionStatus = async (req, res) => {
   const { connectionId } = req.params;
@@ -131,10 +143,14 @@ export const updateConnectionStatus = async (req, res) => {
     connection.status = status;
     await connection.save();
 
-    return res.status(200).json({ message: "Connection status updated successfully." });
-
+    return res
+      .status(200)
+      .json({ message: "Connection status updated successfully." });
   } catch (error) {
-    console.error("[connection.controllers.js] Error updating connection status: ", error.stack);
+    console.error(
+      "[connection.controllers.js] Error updating connection status: ",
+      error.stack
+    );
     return res.status(500).json({
       message: "An error occurred while updating the connection status.",
       error: error.message,
--- server/controllers/message.controllers.js
+++ server/controllers/message.controllers.js
@@ -1,34 +1,39 @@
 import { Message } from "../models/message.models.js";
 import { User } from "../models/user.models.js";
 import { Connection } from "../models/connection.models.js";
-import { Op } from "sequelize"; 
+import { Op } from "sequelize";
+import { getSocketId, io } from "../socket.js";
+import { computedConnection } from "./connection.controllers.js";
 
 export const getMessages = async (req, res) => {
   const { connectionId } = req.params;
 
   try {
     const connection = await Connection.findByPk(connectionId, {
-      where: { status: { [Op.ne]: "blocked" } }
+      where: { status: { [Op.ne]: "blocked" } },
     });
-    if (!connection) return res.status(404).json({ message: "Connection not found!" });
+    if (!connection)
+      return res.status(404).json({ message: "Connection not found!" });
 
     const messages = await Message.findAll({
       where: { connectionId },
-      attributes: ["id", "content", "timestamp"], 
+      attributes: ["id", "content", "timestamp"],
       include: [
         {
           model: User,
           as: "sender",
-          attributes: ["name", "email"], 
+          attributes: ["id"],
         },
       ],
-      order: [["timestamp", "ASC"]], 
+      order: [["timestamp", "ASC"]],
     });
 
     return res.status(200).json(messages);
-
   } catch (error) {
-    console.error("[message.controllers.js] Error fetching messages: ", error.stack);
+    console.error(
+      "[message.controllers.js] Error fetching messages: ",
+      error.stack
+    );
     return res.status(500).json({
       message: "An error occurred while fetching messages.",
       error: error.message,
@@ -37,39 +42,63 @@ export const getMessages = async (req, res) => {
 };
 
 export const sendMessage = async (req, res) => {
-  const { connectionId, senderId, content } = req.body;
+  const { connectionId, senderId, recieverId, content } = req.body;
 
   try {
-    if (!connectionId || !senderId || !content) 
-      return res.status(400).json({ 
-        message: "connectionId, senderId, and content are required.",
+    if (!connectionId || !senderId || !recieverId || !content)
+      return res.status(400).json({
+        message: "connectionId, senderId, recieverId and content are required.",
       });
 
     const connection = await Connection.findByPk(connectionId, {
-      where: { status: { [Op.ne]: "blocked" } }
+      where: { status: { [Op.ne]: "blocked" } },
+      attributes: ["id", "status"],
+      include: [
+        { model: User, as: "user1", attributes: ["id", "name", "email"] },
+        { model: User, as: "user2", attributes: ["id", "name", "email"] },
+      ],
     });
-    if (!connection) return res.status(404).json({ message: "Connection not found!" });
+    if (!connection)
+      return res.status(404).json({ message: "Connection not found!" });
 
-    if(connection.status != "accepted"){
-      switch(connection.status){
+    if (connection.status != "accepted") {
+      switch (connection.status) {
         case "none":
           connection.status = "pending";
+          await connection.save();
+          const newConnection = computedConnection(userId2, connection);
+          const socketId = getSocketId(userId2);
+          if(socketId) io.to(socketId).emit("newConnection", newConnection);
           break;
         case "pending":
-          if(connection.userId2 == senderId) connection.status = "accepted";
+          if (connection.userId2 == senderId){ 
+            connection.status = "accepted";
+            await connection.save();
+          }
           break;
       }
-      await connection.save();
     }
 
-    const newMessage = await Message.create(
-      { connectionId, senderId, content },
-      { attributes: ["id", "content", "timestamp"], raw: true }
-    );
+    const message = await Message.create({ connectionId, senderId, content });
+
+    const newMessage = { 
+      id: message.id,
+      content: message.content,
+      timestamp: message.timestamp,
+      sender: {
+        id: message.senderId,
+      }
+    };
+
+    const socketId = getSocketId(recieverId);
+    if(socketId) io.to(socketId).emit("newMessage", newMessage);
 
     return res.status(201).json(newMessage);
   } catch (error) {
-    console.error("[message.controllers.js] Error sending message: ", error.stack);
+    console.error(
+      "[message.controllers.js] Error sending message: ",
+      error.stack
+    );
     return res.status(500).json({
       message: "An error occurred while sending the message.",
       error: error.message,
@@ -82,14 +111,17 @@ export const deleteMessage = async (req, res) => {
 
   try {
     const message = await Message.findByPk(messageId);
-    if (!message) return res.status(404).json({ message: "Message not found." });
+    if (!message)
+      return res.status(404).json({ message: "Message not found." });
 
     await message.destroy();
 
     return res.status(200).json({ message: "Message deleted successfully." });
-
   } catch (error) {
-    console.error("[message.controllers.js] Error deleting message: ", error.stack);
+    console.error(
+      "[message.controllers.js] Error deleting message: ",
+      error.stack
+    );
     return res.status(500).json({
       message: "An error occurred while deleting the message.",
       error: error.message,
