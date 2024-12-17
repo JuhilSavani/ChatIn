60b5ef1dcebfed86b640be035a86fccf6391c4c4
JuhilSavani <savanijuhil@gmail.com>
2024-12-17 14:06:05 +0530

    move verifyEmail controller out of authorize routes

--- server/routes/authorize.routes.js
+++ server/routes/authorize.routes.js
@@ -1,10 +1,5 @@
 import express from "express";
-import {
-  login,
-  register,
-  logout,
-  verifyEmail,
-} from "../controllers/authorize.controllers.js";
+import { login, register, logout } from "../controllers/authorize.controllers.js";
 import { authenticateJWT } from "../config/passport.config.js";
 
 const router = express.Router();
@@ -12,10 +7,8 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/register", register);
 router.post("/logout", logout);
-router.get("/check", authenticateJWT, (req, res) => {
+router.get("/status", authenticateJWT, (req, res) => {
   res.status(200).json({ message: "Authenticated", user: req.user });
 });
-router.get("/:email", verifyEmail);
-
 
 export default router;
